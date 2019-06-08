a02bcd8770de7d3fa325deeebd0e08fb8aaeb025
日沉云起 <csjiangfeng1212@163.com>
2019-06-08 19:50:07 +0800

    Update 1017. A除以B (20).cpp

--- Basic Level/1017. A除以B (20).cpp
+++ Basic Level/1017. A除以B (20).cpp
@@ -1,20 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char A[1005],Q[1005];
-    int B,R=0,size=0;
-    scanf("%s %d",A,&B);
-    for(int i=0;A[i]!='\0';++i){//大整数除法
-        R=R*10+A[i]-'0';
-        Q[size++]=R/B+'0';
+    string A,Q="";
+    int B,R=0;
+    cin>>A>>B;
+    for(char a:A){//大整数除法
+        R=R*10+a-'0';
+        Q+=R/B+'0';
         R%=B;
     }
-    bool output=false;
-    for(int i=0;i<size;++i)//输出Q并检测Q是否为0，output指示Q是否为0
-        if(Q[i]!='0'||output){
-            printf("%c",Q[i]);
-            output=true;
-        }
-    printf("%s %d",output?"":"0",R);//Q为0先输出一个0再输出R
+    while(!Q.empty()&&Q.front()=='0')//删除Q首部的'0'
+        Q.erase(Q.begin());
+    printf("%s %d",Q.size()==0?"0":Q.c_str(),R);//Q为空先输出一个0再输出R
     return 0;
 }
