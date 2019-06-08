4a4bf3e066cdbf6c46e1fb97f7622f5df49c0d4f
日沉云起 <csjiangfeng1212@163.com>
2019-06-08 19:28:44 +0800

    Update 1003. 我要通过！(20).cpp

--- Basic Level/1003. 我要通过！(20).cpp
+++ Basic Level/1003. 我要通过！(20).cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main(){
     int n;
-    scanf("%d",&n);
-    char num[105];
+    scanf("%d%*c",&n);
+    string num;
     while(n--){
-        scanf("\n%s",num);
+        getline(cin,num);
         int pindex=-1,tindex=-1;
-        for(int i=0;num[i]!='\0';++i)
+        for(int i=0;i<num.size();++i)
             if(num[i]=='P'&&pindex==-1)//当前字符是P字符且P字符没有出现过
                 pindex=i;//更新P字符在字符串中位置
             else if(num[i]=='T'&&tindex==-1)//当前字符是T字符且T字符没有出现过
@@ -16,7 +16,7 @@ int main(){
                 pindex=-2;//置pindex为-2，确保能输出NO
                 break;//跳出循环
             }
-        if(pindex>-1&&tindex>-1&&pindex<tindex-1&&pindex*(tindex-pindex-1)==strlen(num)-1-tindex)
+        if(pindex>-1&&tindex>-1&&pindex<tindex-1&&pindex*(tindex-pindex-1)==num.size()-1-tindex)
             printf("YES\n");//满足所有条件输出YES
         else
             printf("NO\n");
