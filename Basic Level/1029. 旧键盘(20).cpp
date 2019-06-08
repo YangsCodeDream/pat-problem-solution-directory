852102d6e1c64047656cfa0df3661abff94d6592
日沉云起 <csjiangfeng1212@163.com>
2019-06-08 20:21:00 +0800

    Update 1029. 旧键盘(20).cpp

--- Basic Level/1029. 旧键盘(20).cpp
+++ Basic Level/1029. 旧键盘(20).cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char s1[85],s2[85];
-    scanf("%s%s",s1,s2);
+    string s1,s2;
+    cin>>s1>>s2;
     bool hashTable[128]={false};
-    for(int i=0;s2[i]!='\0';++i)
-        hashTable[toupper(s2[i])]=true;
-    for(int i=0;s1[i]!='\0';++i)
+    for(char c:s2)
+        hashTable[toupper(c)]=true;
+    for(int i=0;i<s1.size();++i)
         if(!hashTable[toupper(s1[i])]){
             printf("%c",toupper(s1[i]));
             hashTable[toupper(s1[i])]=true;
