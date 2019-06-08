f8cc43a71b56955e75bbac50093c94d095a0ee5c
日沉云起 <csjiangfeng1212@163.com>
2019-06-08 20:03:32 +0800

    Update 1020. 月饼 (25).cpp

--- Basic Level/1020. 月饼 (25).cpp
+++ Basic Level/1020. 月饼 (25).cpp
@@ -1,13 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct Mooncake{
-    double store;
-    double price;
-    double pricePerStore;
+    double store,price,pricePerStore;
 };
-bool cmp(const Mooncake&m1,const Mooncake&m2){//按单价排序
-    return m1.pricePerStore>m2.pricePerStore;
-}
 int main(){
     int N,D;
     scanf("%d%d",&N,&D);
@@ -18,7 +13,9 @@ int main(){
         scanf("%lf",&mooncake[i].price);
     for(int i=0;i<N;++i)
         mooncake[i].pricePerStore=mooncake[i].price/mooncake[i].store;//计算单价
-    sort(mooncake,mooncake+N,cmp);//按单价排序
+    sort(mooncake,mooncake+N,[](const Mooncake&m1,const Mooncake&m2){
+        return m1.pricePerStore>m2.pricePerStore;
+    });//按单价排序
     double sumPrice=0.0;//卖的最大收益
     for(int i=0;i<N;++i)
         if(D>mooncake[i].store){//D比当前的月饼的库存量大
