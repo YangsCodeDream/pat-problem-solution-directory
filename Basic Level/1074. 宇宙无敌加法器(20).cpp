74a3c51f17fc0c65e21dda6bd662be62e3d38780
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 21:00:18 +0800

    Update 1074. 宇宙无敌加法器(20).cpp

--- Basic Level/1074. 宇宙无敌加法器(20).cpp
+++ Basic Level/1074. 宇宙无敌加法器(20).cpp
@@ -1,27 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char carry[25],n1[25],n2[25];
-    scanf("%s%s%s",carry,n1,n2);
-    int c=0,len1=strlen(n1),len2=strlen(n2),len=max(len1,len2);
-    reverse(carry,carry+strlen(carry));
-    reverse(n1,n1+len1);
-    reverse(n2,n2+len2);
-    ++len;//为了循环中能直接处理最后进位的情况，令len递增
+    string carry,n1,n2,n="";
+    cin>>carry>>n1>>n2;
+    int c=0,len=max(n1.size(),n2.size())+1;//为了循环中能直接处理最后进位的情况，令len为n1,n2最大长度+1
+    reverse(carry.begin(),carry.end());
+    reverse(n1.begin(),n1.end());
+    reverse(n2.begin(),n2.end());
     for(int i=0;i<len;++i){
-        int a=i<len1?n1[i]-'0':0;
-        int b=i<len2?n2[i]-'0':0;
-        int mod=10;
-        if(i<strlen(carry)&&carry[i]!='0')
+        int a=i<n1.size()?n1[i]-'0':0,b=i<n2.size()?n2[i]-'0':0,mod=10;
+        if(i<carry.size()&&carry[i]!='0')
             mod=carry[i]-'0';
-        n1[i]=(a+b+c)%mod+'0';
+        n+=(a+b+c)%mod+'0';
         c=(a+b+c)/mod;
     }
-    while(n1[len-1]=='0')--len;
-    if(len==0)   
-        n1[len++]='0';
-    n1[len]='\0';
-    reverse(n1,n1+len);
-    puts(n1);
+    while(n.size()>1&&n.back()=='0')//删除字符串尾部的'0'字符
+        n.pop_back();
+    reverse(n.begin(),n.end());
+    puts(n.c_str());
     return 0;
 }
