72d26d9c6cfd5512326f1a5b8ff4764c01be5b6f
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 21:47:45 +0800

    Update 1006.Sign In and Sign Out.cpp

--- Advanced Level/1006.Sign In and Sign Out.cpp
+++ Advanced Level/1006.Sign In and Sign Out.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct Person{
-    char id[16];
+    string id="";
     int h,m,s;
     Person(int hh=0,int mm=0,int ss=0):h(hh),m(mm),s(ss){}
     bool operator <(const Person&p)const{//重载小于运算符
@@ -18,11 +18,12 @@ int main(){
     scanf("%d",&N);
     Person p,minTime(INT_MAX,INT_MAX,INT_MAX),maxTime;
     for(int i=0;i<N;++i){
-        scanf("%s %d:%d:%d",p.id,&p.h,&p.m,&p.s);
+        cin>>p.id;
+        scanf("%d:%d:%d",&p.h,&p.m,&p.s);
         minTime=min(minTime,p);
         scanf("%d:%d:%d",&p.h,&p.m,&p.s);
         maxTime=max(maxTime,p);
     }
-    printf("%s %s",minTime.id,maxTime.id);
+    printf("%s %s",minTime.id.c_str(),maxTime.id.c_str());
     return 0;
 }
