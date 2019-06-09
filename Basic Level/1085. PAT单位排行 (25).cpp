5768fd209f9c1bc9df85a5102de2b29e4630cdd5
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 21:22:50 +0800

    Update 1085. PAT单位排行 (25).cpp

--- Basic Level/1085. PAT单位排行 (25).cpp
+++ Basic Level/1085. PAT单位排行 (25).cpp
@@ -5,40 +5,37 @@ struct Testee{
     double TWS=0.0;//加权总分
     string school="";//单位名
 };
-//比较函数
-bool cmp(const Testee&t1,const Testee&t2){
-    if(t1.TWS!=t2.TWS)
-        return t1.TWS>t2.TWS;
-    else if(t1.NS!=t2.NS)
-        return t1.NS<t2.NS;
-    else
-        return t1.school<t2.school;
-}
 int main(){
     int N;
     scanf("%d",&N);
     unordered_map<string,Testee>school;
     while(N--){
-        char id[7],s[7];
+        string id,s;
         double score;
-        scanf("\n%s %lf %s",id,&score,s);
-        for(int i=0;s[i]!='\0';++i)
-            s[i]=tolower(s[i]);//转化为小写
-        string str(s);
+        cin>>id>>score>>s;
+        for(char&c:s)
+            c=tolower(c);//转化为小写
         if(id[0]=='B')
             score/=1.5;
         else if(id[0]=='T')
             score*=1.5;
-        school[str].TWS+=score;//累加加权总分
-        ++school[str].NS;//递增考试人数
-        school[str].school=str;//更新单位名称
+        school[s].TWS+=score;//累加加权总分
+        ++school[s].NS;//递增考试人数
+        school[s].school=s;//更新单位名称
     }
     vector<Testee>v;
-    for(auto i=school.begin();i!=school.end();++i){
-        (i->second).TWS=floor((i->second).TWS);//向下取整
-        v.push_back(i->second);//将数据搬迁到vector中
+    for(auto&i:school){
+        (i.second).TWS=floor((i.second).TWS);//向下取整
+        v.push_back(i.second);//将数据搬迁到vector中
     }
-    sort(v.begin(),v.end(),cmp);//排序
+    sort(v.begin(),v.end(),[](const Testee&t1,const Testee&t2){//比较函数
+        if(t1.TWS!=t2.TWS)
+            return t1.TWS>t2.TWS;
+        else if(t1.NS!=t2.NS)
+            return t1.NS<t2.NS;
+        else
+            return t1.school<t2.school;
+    });//排序
     for(int i=1;i<v.size();++i)
         if(v[i].TWS!=v[i-1].TWS)//当前考生加权总分和排在其前面考生加权总分不一致
             v[i].rank=i+1;//排名为i+1
