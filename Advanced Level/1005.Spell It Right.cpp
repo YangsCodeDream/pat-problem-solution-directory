7971ea1a00c20e658dcc46e7233720d335ceaba0
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 21:44:24 +0800

    Update 1005.Spell It Right.cpp

--- Advanced Level/1005.Spell It Right.cpp
+++ Advanced Level/1005.Spell It Right.cpp
@@ -1,16 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s;
+    string s,temp[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
     cin>>s;
     int sum=0;
     for(int i=0;i<s.size();++i)
         sum+=s[i]-'0';
     s=to_string(sum);
-    char temp[10][6]={
-        "zero","one","two","three","four","five","six","seven","eight","nine"
-    };
     for(int i=0;i<s.size();++i)
-        printf("%s%s",i>0?" ":"",temp[s[i]-'0']);
+        printf("%s%s",i>0?" ":"",temp[s[i]-'0'].c_str());
     return 0;
 }
