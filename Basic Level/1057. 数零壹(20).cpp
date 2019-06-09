7b39a4ebe748de318fff047822f0ce630448f4a0
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 20:38:14 +0800

    Update 1057. 数零壹(20).cpp

--- Basic Level/1057. 数零壹(20).cpp
+++ Basic Level/1057. 数零壹(20).cpp
@@ -1,28 +1,24 @@
 #include<bits/stdc++.h>
-#include<stdlib.h>
 using namespace std;
 int main(){
-    char input[2000005];
-    gets(input);//读取输入数据
+    string input;
+    getline(cin,input);//读取输入数据
     int sum=0;//求英文字母序号之和
-    for(int i=0;input[i]!='\0';++i)
-        if(isalpha(input[i]))
-            sum+=tolower(input[i])-'a'+1;
+    for(char c:input)
+        if(isalpha(c))
+            sum+=tolower(c)-'a'+1;
     if(sum==0){//如果sum为0，输出"0 0"
         printf("0 0");
         return 0;
     }
-    int len=0;
+    int a=0,b=0;
     do{//转化成2进制字符串存储在input中
-        input[len++]=sum%2+'0';
+        if(sum%2==0)
+            ++a;
+        else
+            ++b;
         sum/=2;
     }while(sum!=0);
-    int a=0,b=0;
-    for(int i=0;i<len;++i)//查找0和1的个数
-        if(input[i]=='0')
-            ++a;
-        else if(input[i]=='1')
-            ++b;
     printf("%d %d",a,b);//输出
     return 0;
 }
