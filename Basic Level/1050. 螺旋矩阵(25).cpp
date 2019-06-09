48d50a8939003bba771f93c7ebbcc8eaaee38e13
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 20:21:53 +0800

    Update 1050. 螺旋矩阵(25).cpp

--- Basic Level/1050. 螺旋矩阵(25).cpp
+++ Basic Level/1050. 螺旋矩阵(25).cpp
@@ -1,45 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int N;
+    int N,m,n;//m记录行数，n记录列数
     scanf("%d",&N);
     int a[N];
     for(int i=0;i<N;++i)
         scanf("%d",&a[i]);
     sort(a,a+N);//排序，注意这是按从小到大的顺序排序
-    int m,n;//m记录行数，n记录列数
-    //求出m,n
-    for(n=(int)sqrt(N*1.0);n>=0;--n){
+    for(n=(int)sqrt(N*1.0);n>=0;--n)//求出m,n
         if(N%n==0){
             m=N/n;
             break;
         }
-    }
-    //定义要填充的二维数组
-    int matrix[m][n];
-    //初始化
-    for(int i=0;i<m;++i)
+    int matrix[m][n];//定义要填充的二维数组
+    for(int i=0;i<m;++i)//初始化
         for(int j=0;j<n;++j)
             matrix[i][j]=0;
-    //定义左上右下的方向数组
-    int direction[4][2]={
-        {0,1},{1,0},{0,-1},{-1,0}
-    };
-    //当前方向
-    int direct=0;
-    int i=0,j=-1;
-    //进行填充
-    for(int k=N-1;k>=0;--k){
-        //记录将要填充的位置
-        int ii=i+direction[direct%4][0],jj=j+direction[direct%4][1];
+    
+    int direction[4][2]={//定义左上右下的方向数组
+        {0,1},{1,0},{0,-1},{-1,0}};
+    int direct=0,i=0,j=-1;
+    for(int k=N-1;k>=0;--k){ //进行填充
+        int ii=i+direction[direct%4][0],jj=j+direction[direct%4][1];//记录将要填充的位置
         if(ii<0||ii>=m||jj<0||jj>=n||matrix[ii][jj]!=0)
             direct+=1;//如果将要填充的位置超出了matrix的维度或者已被填充,改变方向
         i+=direction[direct%4][0];
         j+=direction[direct%4][1];
         matrix[i][j]=a[k];
     }
-    //输出
-    for(i=0;i<m;++i){
+    
+    for(i=0;i<m;++i){//输出
         for(j=0;j<n;++j){
             if(j>0)
                 printf(" ");
