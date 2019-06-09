601de7f4effeecf6b38d464d87bd6266eed0fb6f
日沉云起 <csjiangfeng1212@163.com>
2019-06-09 18:11:26 +0800

    Update 1041. 考试座位号(15).cpp

--- Basic Level/1041. 考试座位号(15).cpp
+++ Basic Level/1041. 考试座位号(15).cpp
@@ -1,21 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int N;
+    int N,b,c;
     scanf("%d",&N);
-    long long id[1005],testid[1005];
+    long long id[1005],testid[1005],a;
     for(int i=0;i<N;++i){
-        long long a;
-        int b,c;
         scanf("%lld%d%d",&a,&b,&c);
         id[b]=a;
         testid[b]=c;
     }
     scanf("%d",&N);
     while(N--){
-        int a;
-        scanf("%d",&a);
-        printf("%014lld %d\n",id[a],testid[a]);
+        scanf("%d",&b);
+        printf("%014lld %d\n",id[b],testid[b]);
     }
     return 0;
 }
