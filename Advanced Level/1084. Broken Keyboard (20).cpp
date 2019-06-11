f046da08d526cb01277c989ee0af5269b78694aa
日沉云起 <csjiangfeng1212@163.com>
2019-06-11 21:27:39 +0800

    Update 1084. Broken Keyboard (20).cpp

--- Advanced Level/1084. Broken Keyboard (20).cpp
+++ Advanced Level/1084. Broken Keyboard (20).cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char s1[85],s2[85];
-    scanf("%s%s",s1,s2);
-    unordered_set<char>s;
-    for(int i=0;s2[i]!='\0';++i)
-        s.insert(toupper(s2[i]));
-    for(int i=0;s1[i]!='\0';++i)
-        if(s.find(toupper(s1[i]))==s.cend()){
+    string s1,s2;
+    cin>>s1>>s2;
+    bool hashTable[128]={false};
+    for(char c:s2)
+        hashTable[toupper(c)]=true;
+    for(int i=0;i<s1.size();++i)
+        if(!hashTable[toupper(s1[i])]){
             printf("%c",toupper(s1[i]));
-            s.insert(toupper(s1[i]));
+            hashTable[toupper(s1[i])]=true;
         }
     return 0;
 }
