6951333f20621249d51b82166c48ef1d4024daca
日沉云起 <csjiangfeng1212@163.com>
2019-06-11 19:37:32 +0800

    Update 1023. Have Fun with Numbers (20).cpp

--- Advanced Level/1023. Have Fun with Numbers (20).cpp
+++ Advanced Level/1023. Have Fun with Numbers (20).cpp
@@ -1,31 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-char input[25],result[25];//输入的大整数、2倍后的大整数
+string input,result="";//输入的大整数、2倍后的大整数
 int hashTable[10];//辅助数组
-int mul(){//大整数乘法
-    int len=strlen(input),num=0;//进位num初始化为0
-    result[len]='\0';//将新数相应位置置为'\0'字符，方便输出
-    for(int i=len-1;i>=0;--i){//从个位开始乘2
-        int k=2*(input[i]-'0')+num;//获取每一位乘2的值
-        result[i]=k%10+'0';//取个位字符
-        num=k/10;//将十位数作为进位
-        ++hashTable[input[i]-'0'];//在相应的hashTable数组下标下进行递增
-        --hashTable[result[i]-'0'];//在相应的hashTable数组下标下进行递减
+void mul(){//大整数乘法
+    int num=0;
+    for(int i=0;input[i]!='\0';++i){
+        int k=2*(input[i]-'0')+num;
+        result+=k%10+'0';
+        num=k/10;
     }
-    return num;//返回最终进位
+    if(num!=0)
+        result+=num+'0';//如果最终进位不为0，置于result数组末尾
 }
 int main(){
-    gets(input);
-    int num=mul();//获得最终进位
-    bool yes=true;//输出Yes或者No的标志
-    if(num!=0)
-        --hashTable[num];
-    for(int i=0;i<10&&yes;++i)//遍历整个hashTable数组，看是否有不为0的元素，如果有置yes为false
-        if(hashTable[i]!=0)
-            yes=false;
-    printf("%s\n",num==0&&yes?"Yes":"No");
-    if(num!=0)//如果进位不是0，先输出进位
-        printf("%d",num);
-    printf("%s",result);//输出新数
+    cin>>input;
+    reverse(input.begin(),input.end());//翻转读取到的数组
+    mul();//将原数加倍
+    for(int i=0;input[i]!='\0';++i){//遍历新数原数两个数组，在相应的hashTable数组下标下进行递增递减
+        ++hashTable[input[i]-'0'];
+        --hashTable[result[i]-'0'];
+    }
+    bool yes=(count(hashTable,hashTable+10,0)==10);//遍历整个hashTable数组，看元素0的个数是否为10
+    reverse(result.begin(),result.end());//将result数组翻转
+    printf("%s\n%s",yes?"Yes":"No",result.c_str());
     return 0;
 }
