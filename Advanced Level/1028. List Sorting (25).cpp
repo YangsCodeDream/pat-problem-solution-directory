7d595522885b96e47bd021d48f2ef522e2c21b60
日沉云起 <csjiangfeng1212@163.com>
2019-06-11 19:53:51 +0800

    Update 1028. List Sorting (25).cpp

--- Advanced Level/1028. List Sorting (25).cpp
+++ Advanced Level/1028. List Sorting (25).cpp
@@ -2,30 +2,27 @@
 using namespace std;
 struct Student{
     int id,grade;
-    char name[10];
+    string name;
 };
-bool cmp1(const Student&s1,const Student&s2){
-    return s1.id<s2.id;
-}
-bool cmp2(const Student&s1,const Student&s2){
-    return strcmp(s1.name,s2.name)!=0?strcmp(s1.name,s2.name)<0:s1.id<s2.id;
-}
-bool cmp3(const Student&s1,const Student&s2){
-    return s1.grade!=s2.grade?s1.grade<s2.grade:s1.id<s2.id;
-}
 int main(){
     int N,C;
     scanf("%d%d",&N,&C);
     vector<Student>stu(N);
     for(int i=0;i<N;++i)
-        scanf("%d %s %d",&stu[i].id,stu[i].name,&stu[i].grade);
+        cin>>stu[i].id>>stu[i].name>>stu[i].grade;
     if(C==1)
-        sort(stu.begin(),stu.end(),cmp1);
+        sort(stu.begin(),stu.end(),[](const Student&s1,const Student&s2){
+            return s1.id<s2.id;
+        });
     else if(C==2)
-        sort(stu.begin(),stu.end(),cmp2);
+        sort(stu.begin(),stu.end(),[](const Student&s1,const Student&s2){
+            return s1.name!=s2.name?s1.name<s2.name:s1.id<s2.id;
+        });
     else if(C==3)
-        sort(stu.begin(),stu.end(),cmp3);
+        sort(stu.begin(),stu.end(),[](const Student&s1,const Student&s2){
+            return s1.grade!=s2.grade?s1.grade<s2.grade:s1.id<s2.id;
+        });
     for(int i=0;i<N;++i)
-        printf("%06d %s %d\n",stu[i].id,stu[i].name,stu[i].grade);
+        printf("%06d %s %d\n",stu[i].id,stu[i].name.c_str(),stu[i].grade);
     return 0;
 }
